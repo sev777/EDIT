e84f440b9b4d857ff8d9d87251364e59b31d97c2
yzc <419432571@qq.com>
2021-09-03 19:42:18 +0800

    9-3-check all

--- scripts/eval_bert_binary.py
+++ scripts/eval_bert_binary.py
@@ -247,13 +247,13 @@ if __name__ == "__main__":
                 d0["cond"],
                 logits_orig,
                 params_dict,
-                stop_condition=lambda condition, logits, n_iter: (
-                                                                         ("REFUTES >> SUPPORTS" in condition and logits[
-                                                                             -1] < 0)
-                                                                         or ("SUPPORTS >> REFUTES" in condition and
-                                                                             logits[-1] > 0)
-                                                                 )
-                                                                 and n_iter < 5,
+                # stop_condition=lambda condition, logits, n_iter: (
+                #                                                          ("REFUTES >> SUPPORTS" in condition and logits[
+                #                                                              -1] < 0)
+                #                                                          or ("SUPPORTS >> REFUTES" in condition and
+                #                                                              logits[-1] > 0)
+                #                                                  )
+                #                                                  and n_iter < 5,
             )
 
             all_rephrases[j] = logits.cpu()
@@ -373,13 +373,13 @@ if __name__ == "__main__":
 
 
 
-        for mode in ['row','max','mean','sum']:
+        for mode in ['max','mean','sum','min']:#'row','max','mean','sum'
             all_logits = {}
             all_rephrases = {}
             right = {'s0': [], 's1': [], 'r0': [], 'r1': [], 'e0': [], 'e1': []}
             iter_ = tqdm(val_dataset0, ncols=200, desc=mode)
             print(mode)
-            precesson, param_stack = 0, []
+            precesson, param_stack,data_stack = 0, [],[]
             for j, d0 in iter_:
 
                 with torch.no_grad():
@@ -397,6 +397,7 @@ if __name__ == "__main__":
                 if precesson != 4:
                     precesson+=1
                     param_stack.append(params_dict)
+                    data_stack.append([j,d0])
                     continue
                 # print()
                 if mode=='max':
@@ -404,6 +405,12 @@ if __name__ == "__main__":
                     params_dict0={n:torch.max(param_stack[0][n],param_stack[1][n])  for n in param_stack[0]}
                     params_dict1={n:torch.max(param_stack[2][n],param_stack[3][n]) for n in param_stack[0]}
                     params_dict={n:torch.max(params_dict0[n],params_dict1[n]) for n in param_stack[0]}
+                    params_dict1,params_dict0={},{}
+                if mode=='min':
+                #max
+                    params_dict0={n:torch.min(param_stack[0][n],param_stack[1][n])  for n in param_stack[0]}
+                    params_dict1={n:torch.min(param_stack[2][n],param_stack[3][n]) for n in param_stack[0]}
+                    params_dict={n:torch.min(params_dict0[n],params_dict1[n]) for n in param_stack[0]}
                     params_dict1,params_dict0={},{}
                     # params_dict={p: torch.max(torch.stack([param_stack[i][p] for i in range(precesson)], dim=0), dim=0) for p in param_stack[0]}
                     # params_dict = {p: max(param_stack[i][p] for i in range(precesson)) for p in param_stack[0]}
@@ -427,50 +434,55 @@ if __name__ == "__main__":
 
                 param_stack=[]
                 precesson = 0
-                _, logits, params_dict = model.sample(
-                    d0["view"],
-                    d0["cond"],
-                    logits_orig,
-                    params_dict,
-                    stop_condition=lambda condition, logits, n_iter: (
-                        ("REFUTES >> SUPPORTS" in condition and logits[-1] < 0)
-                        or ("SUPPORTS >> REFUTES" in condition and logits[-1] > 0)
-                    )
-                    and n_iter < 5,
-                )
-
-                all_rephrases[j] = logits.cpu()
-
-                all_logits_batch = []
-                for i, d1 in enumerate(batch_it(val_dataset1, args.batch_size)):
-                    _, logits, _ = model.sample(
-                        [e["src"] for e in d1], d0["cond"],logits_orig, params_dict
-                    )
-                    all_logits_batch.append(logits.cpu())
-
-                all_logits[j] = torch.cat(all_logits_batch)
-
-                iter_.set_postfix(
-                    succ=sum(
-                        val_dataset1[k]["alt"] == (v[k] > 0).item()
-                        for k, v in all_logits.items()
-                    )
-                    / len(all_logits),
-                    retain=sum(
-                        (
-                            ((v[:k] > 0) == preds[:k]).sum()
-                            + ((v[k + 1 :] > 0) == preds[k + 1 :]).sum()
+                for j,d0 in data_stack:
+                    _, logits, params_dict = model.sample(
+                        d0["view"],
+                        d0["cond"],
+                        logits_orig,
+                        params_dict,
+                        stop_condition=lambda condition, logits, n_iter: (
+                            ("REFUTES >> SUPPORTS" in condition and logits[-1] < 0)
+                            or ("SUPPORTS >> REFUTES" in condition and logits[-1] > 0)
                         )
-                        / (len(v) - 1)
-                        for k, v in all_logits.items()
-                    ).item()
-                    / len(all_logits),
-                    equiv=sum(
-                        (v.sign() == all_logits[k][k].sign()).float().mean().item()
-                        for k, v in all_rephrases.items()
+                        and n_iter < 5,
                     )
-                    / len(all_rephrases),
-                )
+
+                    all_rephrases[j] = logits.cpu()
+
+                    all_logits_batch = []
+                    for i, d1 in enumerate(batch_it(val_dataset1, args.batch_size)):
+                        _, logits, _ = model.sample(
+                            [e["src"] for e in d1], d0["cond"],logits_orig, params_dict
+                        )
+                        all_logits_batch.append(logits.cpu())
+
+                    all_logits[j] = torch.cat(all_logits_batch)
+
+                    iter_.set_postfix(
+                        succ=sum(
+                            val_dataset1[k]["alt"] == (v[k] > 0).item()
+                            for k, v in all_logits.items()
+                        )
+                        / len(all_logits),
+                        retain=sum(
+                            (
+                                ((v[:k] > 0) == preds[:k]).sum()
+                                + ((v[k + 1 :] > 0) == preds[k + 1 :]).sum()
+                            )
+                            / (len(v) - 1)
+                            for k, v in all_logits.items()
+                        ).item()
+                        / len(all_logits),
+                        equiv=sum(
+                            (v.sign() == all_logits[k][k].sign()).float().mean().item()
+                            for k, v in all_rephrases.items()
+                        )
+                        / len(all_rephrases),
+                        len_logits=len(all_logits),
+                        len_rephrases=len(all_rephrases)
+                    )
+                data_stack=[]
+
         #
         # for (k, v),v1 in zip(all_logits.items(),val_dataset1):
         #
@@ -532,4 +544,31 @@ row: 1000/1000 [03:09<00:00,  5.28it/s, equiv=1, retain=0.371, succ=0.545]
 sum:  1000/1000 [02:55<00:00,  5.70it/s, equiv=1, retain=0.323, succ=0.65]
 mean:  1000/1000 [03:03<00:00,  5.45it/s, equiv=0.831, retain=0.819, succ=0.95]
 max:  1000/1000 [02:55<00:00,  5.70it/s, equiv=0.9, retain=0.673, succ=0.98]
+
+“pred” = orinal PREDiction from the model; “alt” = ALTernative prediction we want the model to get; “cond” = CONDition we use for the update (aka input of the hypernetwork)
+for binary predictions when ‘pred’ is TRUE ,‘alt’ is FALSE and the other way around. for QA “pred” is the answer the model outputs and “alt” is the alternative prediction we want the model to make
+'''
+
+'''
+row: one by one equiv=0.998, retain=0.372, succ=0.625]
+
+
+  warnings.warn(_create_warning_msg(
+max:   0%|                                                                                                                                                                     | 0/1000 [00:00<?, ?it/s]max
+max: 100%|████████████████████████████████████████████████████████████████████████████████| 1000/1000 [09:48<00:00,  1.70it/s, equiv=0.874, len_logits=800, len_rephrases=800, retain=0.621, succ=0.899]
+mean:   0%|                                                                                                                                                                    | 0/1000 [00:00<?, ?it/s]mean
+mean: 100%|███████████████████████████████████████████████████████████████████████████████| 1000/1000 [10:06<00:00,  1.65it/s, equiv=0.845, len_logits=800, len_rephrases=800, retain=0.638, succ=0.892]
+sum:   0%|                                                                                                                                                                     | 0/1000 [00:00<?, ?it/s]sum
+sum: 100%|████████████████████████████████████████████████████████████████████████████████| 1000/1000 [10:06<00:00,  1.65it/s, equiv=0.862, len_logits=800, len_rephrases=800, retain=0.522, succ=0.812]
+'''
+
+'''
+max:   0%|                                                                                                                                                                     | 0/1000 [00:00<?, ?it/s]max
+max: 100%|████████████████████████████████████████████████████████████████████████████████| 1000/1000 [09:33<00:00,  1.74it/s, equiv=0.874, len_logits=800, len_rephrases=800, retain=0.621, succ=0.899]
+mean:   0%|                                                                                                                                                                    | 0/1000 [00:00<?, ?it/s]mean
+mean: 100%|███████████████████████████████████████████████████████████████████████████████| 1000/1000 [10:00<00:00,  1.67it/s, equiv=0.845, len_logits=800, len_rephrases=800, retain=0.638, succ=0.892]
+sum:   0%|                                                                                                                                                                     | 0/1000 [00:00<?, ?it/s]sum
+sum: 100%|████████████████████████████████████████████████████████████████████████████████| 1000/1000 [10:01<00:00,  1.66it/s, equiv=0.862, len_logits=800, len_rephrases=800, retain=0.522, succ=0.812]
+min:   0%|                                                                                                                                                                     | 0/1000 [00:00<?, ?it/s]min
+min: 100%|████████████████████████████████████████████████████████████████████████████████| 1000/1000 [09:54<00:00,  1.68it/s, equiv=0.853, len_logits=800, len_rephrases=800, retain=0.634, succ=0.891]
 '''
